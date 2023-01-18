d47ceebcb1a01ac261e1df10222f171bd2ab03eb
ulkio <guillaume.verschave@gmail.com>
2023-01-18 12:30:05 +0100

    fix horizontal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center  h-16  w-screen  bg-blue-palette-navbar ">
+    <footer className="flex items-center justify-center  h-16    bg-blue-palette-navbar ">
       © 2023 Guillaume Verschave
     </footer>
   );
