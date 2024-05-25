62cd2d47c5977892f84d7730383f0bbdf6864409
Guillaume <guillaume.verschave@gmail.com>
2024-05-25 23:35:03 +0200

    fixed iframe errors

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -51,10 +51,6 @@ const Contact = () => {
                 <iframe
                   width="520"
                   height="400"
-                  frameborder="0"
-                  scrolling="no"
-                  marginheight="0"
-                  marginwidth="0"
                   id="gmap_canvas"
                   src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=4%20SAINT%20VICTURNIEN+()&amp;t=&amp;z=10&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
                 <script
